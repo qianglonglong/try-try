4d7953ba0349d855617b9de0a2a6b9742ffb74d1
lorne.Qiang <lorne.qiang@artcare.com>
2017-01-03 17:37:17 +0800

    测试提交

--- TEXT/ViewController.m
+++ TEXT/ViewController.m
@@ -10,20 +10,33 @@
 
 @interface ViewController ()
 
+@property (nonatomic) int sum;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    int a = 5;
+    int b = 10;
+    
+    self.sum = a + b;
+    
+    NSLog(@"The result is: %d", self.sum);
 }
+
+
+
 
 
 @end
