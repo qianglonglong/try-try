dcbf9431d1640f4cb58853215f4744fbac894dde
lorne.Qiang <lorne.qiang@artcare.com>
2017-01-03 15:22:04 +0800

    add all files

---
+++ TEXT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEXT
+//
+//  Created by 强龙 on 17/1/3.
+//  Copyright © 2017年 YZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TEXT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEXT
+//
+//  Created by 强龙 on 17/1/3.
+//  Copyright © 2017年 YZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
